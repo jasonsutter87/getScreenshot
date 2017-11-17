25abc88251a98dd5f0dd2323046ae4ada28bcfd7
Jason Sutter <jasonsutter@Rocko.wavecable.com>
2017-11-17 14:12:55 -0800

    Implement html2canvas

--- getScreenshot.js
+++ getScreenshot.js
@@ -1,4 +1,10 @@
-function GetScreenshot(e) {
-  e.preventDefault();
-  console.log('clicking')
+function GetScreenshot() {
+  html2canvas(window.parent.document.getElementsByTagName('dd-map'), {
+  logging: false,
+  useCORS: true,
+  dpi: 192,
+  onrendered: function(canvas) {
+      document.body.appendChild(canvas);
+    }
+  });
 }
